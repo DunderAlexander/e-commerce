3df9dc85049b89d25ad967fd907d8a605f2c51a2
DunderAlexander <mego.sanello@gmail.com>
2023-01-16 22:51:25 +0400

    added + and - buttons, as well as "proceed" button

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import { setIsCartOpen } from "../redux/slices/utilitySlice";
 import { RootState } from "../redux/store";
 import CartItem from "./CartItem";
 
@@ -10,12 +11,20 @@ const Cart = () => {
     return acc + item!.price * curr.quantity;
   }, 0);
   return (
-    <div className="fixed top-20 right-0 w-64 h-64 bg-slate-400 rounded-lg p-4 pb-8 z-10">
-      <h2 className="text-lg font-medium">My Cart</h2>
+    <div className="fixed top-20 right-0 w-[26rem] h-[30rem] bg-slate-100 rounded-lg p-4 pb-8 z-10">
+      <div className="flex justify-between items-center">
+        <h2 className="text-lg font-medium">My Cart</h2>
+        <button
+          className="font-bold"
+          onClick={() => dispatch(setIsCartOpen(false))}
+        >
+          X
+        </button>
+      </div>
       {cart.length === 0 ? (
         <h3>Your cart is empty.</h3>
       ) : (
-        <div className="flex flex-col gap-2 h-full overflow-y-scroll">
+        <div className="flex flex-col gap-2 h-full overflow-y-auto">
           <ul className="flex flex-col gap-2 pb-3">
             {cart.map((cartItem) => (
               <CartItem key={cartItem.id} {...cartItem} />
@@ -23,6 +32,9 @@ const Cart = () => {
           </ul>
           <div className="w-full flex justify-between items-center py-2">
             <h3 className="text-lg font-medium">Total: ${totalCost}</h3>
+            <button className="bg-blue-500 text-white p-2 rounded-sm">
+              Proceed
+            </button>
           </div>
         </div>
       )}
--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -1,5 +1,6 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../redux/store";
+import { useDispatch, useSelector } from "react-redux";
+import { addToCart, removeFromCart } from "../redux/slices/cartSlice";
+import { AppDispatch, RootState } from "../redux/store";
 
 type CartItemProps = {
   id: number;
@@ -8,15 +9,32 @@ type CartItemProps = {
 
 const CartItem = ({ id, quantity }: CartItemProps) => {
   const storageItems = useSelector((state: RootState) => state.items);
+  const dispatch = useDispatch<AppDispatch>();
   const item = storageItems.find((i) => i.id === id);
   return (
-    <div className="flex items-center py-2">
-      <img src={item?.img} alt={item?.name} className="w-8 rounded-sm mr-2" />
-      <div className="flex flex-col">
-        <p className="text-lg">{item?.name}</p>
-        <p className="text-sm text-gray-600">
-          x{quantity} ${item!.price * quantity}
-        </p>
+    <div className="flex items-center py-2  justify-between">
+      <div className="flex items-center">
+        <img src={item?.img} alt={item?.name} className="w-8 rounded-sm mr-2" />
+        <div className="flex flex-col">
+          <p className="text-lg">{item?.name}</p>
+          <p className="text-sm text-gray-600">
+            x{quantity} ${item!.price * quantity}
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center gap-2">
+        <button
+          className="bg-blue-500 text-white p-2 w-10 rounded-md"
+          onClick={() => dispatch(addToCart(item))}
+        >
+          +
+        </button>
+        <button
+          className="bg-blue-500 text-white p-2 w-10 rounded-md"
+          onClick={() => dispatch(removeFromCart(item))}
+        >
+          -
+        </button>
       </div>
     </div>
   );
