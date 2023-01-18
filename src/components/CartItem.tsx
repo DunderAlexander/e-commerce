7381583acdbbf171cf1179f9d0c4acdf540e4645
DunderAlexander <mego.sanello@gmail.com>
2023-01-18 17:37:18 +0400

    cartpage now shows items in the cart w/ type

--- src/components/CartItem.tsx
+++ src/components/CartItem.tsx
@@ -5,18 +5,26 @@ import { AppDispatch, RootState } from "../redux/store";
 type CartItemProps = {
   id: number;
   quantity: number;
+  showCategory?: boolean;
 };
 
-const CartItem = ({ id, quantity }: CartItemProps) => {
+const CartItem = ({ id, quantity, showCategory }: CartItemProps) => {
   const storageItems = useSelector((state: RootState) => state.items);
   const dispatch = useDispatch<AppDispatch>();
   const item = storageItems.find((i) => i.id === id);
   return (
     <div className="flex items-center py-2  justify-between">
       <div className="flex items-center">
-        <img src={item?.img} alt={item?.name} className="w-8 rounded-sm mr-2" />
+        <img
+          src={item?.img}
+          alt={item?.name}
+          className={`w-${showCategory ? 64 : 8} rounded-sm mr-2`}
+        />
         <div className="flex flex-col">
           <p className="text-lg">{item?.name}</p>
+          {showCategory && (
+            <p className="text-sm text-gray-600">{item?.type}</p>
+          )}
           <p className="text-sm text-gray-600">
             x{quantity} ${item!.price * quantity}
           </p>
--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,5 +1,36 @@
+import { useSelector } from "react-redux";
+import CartItem from "../components/CartItem";
+import { RootState } from "../redux/store";
+
 const CartPage = () => {
-  return <div>CartPage</div>;
+  const { items, cart } = useSelector((state: RootState) => state);
+  const totalCost = cart.reduce((acc, curr) => {
+    const item = items.find((i) => i.id === curr.id);
+    return acc + item!.price * curr.quantity;
+  }, 0);
+  return (
+    <div className="bg-gray-200">
+      <div className="px-4 py-2">
+        <h2 className="text-3xl font-medium text-center pt-4">My Cart</h2>
+      </div>
+      {cart.length === 0 ? (
+        <h3 className="text-center text-xl font-medium text-gray-500 pb-4">
+          Your cart is empty.
+        </h3>
+      ) : (
+        <div className="px-4 py-2">
+          {cart.map((cartItem) => (
+            <div className="bg-white rounded-md my-2 p-2">
+              <CartItem key={cartItem.id} showCategory={true} {...cartItem} />
+            </div>
+          ))}
+          <div className="w-full flex justify-between items-center py-2">
+            <h3 className="text-xl font-medium">Total: ${totalCost}</h3>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default CartPage;
