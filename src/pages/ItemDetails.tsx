3414880e51f25233fb7da93bcd0011be202465a1
DunderAlexander <mego.sanello@gmail.com>
2023-01-31 15:28:43 +0400

    addToCart no longer throws "id is undefined"

--- src/pages/ItemDetails.tsx
+++ src/pages/ItemDetails.tsx
@@ -7,38 +7,38 @@ import { useMemo } from "react";
 const ItemDetails = () => {
   const { id } = useParams();
   const itemId = typeof id === "string" ? parseInt(id) : undefined;
-  const selectedItem = useSelector((state: RootState) =>
+  const item = useSelector((state: RootState) =>
     state.items.find((i) => i.id === itemId)
   );
   const cart = useSelector((state: RootState) => state.cart);
   const uid = useSelector((state: RootState) => state.userAccount.user?.uid);
   const dispatch = useDispatch();
 
-  if (!selectedItem) {
+  if (!item) {
     return <h3 className="text-xl font-medium text-center">Item not found</h3>;
   }
   const clickedItem = useMemo(
-    () => cart.find((i) => i.id === selectedItem.id),
-    [cart, selectedItem.id]
+    () => cart.find((i) => i.id === item.id),
+    [cart, item.id]
   );
 
   return (
     <div className="grid grid-cols-2 gap-4 p-4">
       <img
         className="w-64 h-64 object-cover rounded-md"
-        src={selectedItem.img}
-        alt={selectedItem.name}
+        src={item.img}
+        alt={item.name}
       />
       <div className="self-center">
-        <h1 className="text-3xl font-medium">{selectedItem.name}</h1>
-        <p className="text-gray-600">{selectedItem.description}</p>
-        <h3 className="text-xl font-medium">${selectedItem.price}</h3>
+        <h1 className="text-3xl font-medium">{item.name}</h1>
+        <p className="text-gray-600">{item.description}</p>
+        <h3 className="text-xl font-medium">${item.price}</h3>
         <button
           className={`bg-indigo-500 hover:bg-indigo-600 text-white py-2 px-4 rounded-lg ${
             clickedItem && "opacity-50 cursor-not-allowed hover:bg-indigo-500"
           }`}
           onClick={() => {
-            dispatch(addToCart({ selectedItem, uid }));
+            dispatch(addToCart({ item, uid }));
           }}
           disabled={!!clickedItem}
         >
