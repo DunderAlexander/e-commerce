e6d3985e4ec32dc95eaed3f2bf729863428dcbfa
DunderAlexander <mego.sanello@gmail.com>
2023-02-17 18:17:37 +0400

    gold stars!

--- src/components/Review.tsx
+++ src/components/Review.tsx
@@ -1,17 +1,29 @@
 import { SingleReview } from "../redux/slices/itemsSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar } from "@fortawesome/free-solid-svg-icons";
 
 type ReviewType = {
   content: SingleReview;
 };
 
 const Review: React.FC<ReviewType> = ({ content }) => {
+  const { userName, rating, review } = content;
+
+  const goldenStars = Array(rating).fill(
+    <FontAwesomeIcon key={rating} icon={faStar} color="#ffc107" />
+  );
+  const grayStars = Array(5 - rating).fill(
+    <FontAwesomeIcon key={5 - rating} icon={faStar} color="#c4c4c4" />
+  );
+
   return (
-    <div className="bg-white p-4 shadow-md rounded-lg">
-      <div className="text-sm font-medium">{content.userName}</div>
-      <div className="text-sm font-medium">
-        {Array(content.rating).fill("★").join("")}
+    <div className="bg-white p-4 shadow-md rounded-lg max-w-xl">
+      <div className="text-base font-medium mb-2">{userName}</div>
+      <div className="flex items-center mb-2">
+        {goldenStars}
+        {grayStars}
       </div>
-      <div className="text-sm mt-2">{content.review}</div>
+      <div className="text-base">{review}</div>
     </div>
   );
 };
